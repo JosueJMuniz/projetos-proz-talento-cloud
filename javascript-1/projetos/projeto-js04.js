fc402267cb77cc3d26490bc59b074daf4b0a24b1
Josué Muniz <josue.office01@gmail.com>
2023-11-11 22:38:15 -0300

    Add files via upload

---
+++ javascript-1/projetos/projeto-js04.js
@@ -0,0 +1,32 @@
+function recebeNum(numA, numB) {
+    let frase1 = `${numA} + ${numB} = ${numA + numB}`
+    let frase2 = `${numA} + ${numB} = ${numA - numB}`
+    let frase3 = `${numA} + ${numB} = ${numA * numB}`
+    let frase4 = `${numA} + ${numB} = ${numA / numB}`
+    console.log(frase1);
+    console.log(frase2);
+    console.log(frase3);
+    console.log(frase4);
+}
+
+recebeNum(4, 5);
+
+/* Outra forma de fazer isso */
+function operacoes(num1, num2) {
+
+    op1 = num1 + num2;
+
+    op2 = num1 - num2;
+
+    op3 = num1 * num2;
+
+    op4 = num1 / num2;
+
+    console.log(`
+    Adição: 4+5 = ${op1}
+    Subtração: 4-5 = ${op2}
+    Multiplicação: 4x5 = ${op3}
+    Divisão: 4/5 = ${op4}`);
+}
+
+operacoes(4, 5);
