e1101632f38c2a3447c4bf9a6889ec547087021d
Josué Muniz <josue.office01@gmail.com>
2024-01-12 00:29:50 -0300

    Update banco-de-dados-escola.sql

--- banco-de-dados-1/exercicios-banco-de-dados1/banco-de-dados-escola.sql
+++ banco-de-dados-1/exercicios-banco-de-dados1/banco-de-dados-escola.sql
@@ -1,1 +1,10 @@
-rename table alunos to Aluno;
+create database escola;
+
+use escola;
+
+create table Aluno(
+  	id int unsigned not null auto_increment primary key,
+    nome varchar(35) not null,
+    email varchar(50),
+    endereco varchar(60)
+);
