9f9878da80ca21f7b94aff30adde899eead9abf6
Josué Muniz <josue.office01@gmail.com>
2024-01-12 00:27:53 -0300

    Add files via upload

---
+++ banco-de-dados-1/exercicios-banco-de-dados1/Teste1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Alunos(
+			 matricula int(11),
+             nome char(20) NOT NULL,
+             endereco char(30),
+             
+PRIMARY KEY (matricula));
+
+ 
---
+++ banco-de-dados-1/exercicios-banco-de-dados1/banco-de-dados-escola.sql
@@ -0,0 +1,1 @@
+rename table alunos to Aluno;
